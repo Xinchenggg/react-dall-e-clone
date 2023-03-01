7d5f88f1f7af1f9853233108a11701561af44a82
Xinchenggg <xinchengni26@gmail.com>
2023-03-01 19:10:15 +0200

    First release
    First version release

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # react-dall-e-clone
-  Practice of making a clone website of DALL-E with React
+
+## Intro
+This is a practice of making a clone web application of DALL-E with MERN stack.
+
+
+
+By default configuration, the application will generate `1` image in `b64_json` format, with resolution of `256x256`, the other accepted size are `512x512` and `1024x1024`. You can change it in `\server\routes\dalleRoutes.js`
+
+Note that each OpenAi account has $18 free trial usage, more generated image and larger size will cost more credit.
+
+Besides, have fun!😜
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
@@ -15,10 +14,61 @@ const CreatePost = () => {
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-	const generateImage = () => {};
+	const generateImage = async () => {
+		if (form.prompt) {
+			try {
+				setGeneratingImg(true);
+				const response = await fetch(
+					'http://localhost:8080/api/v1/dalle',
+					{
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify({ prompt: form.prompt }),
+					}
+				);
+				const data = await response.json();
+				setForm({
+					...form,
+					photo: `data:image/jpeg;base64,${data.photo}`,
+				});
+			} catch (error) {
+				alert(error);
+			} finally {
+				setGeneratingImg(false);
+			}
+		} else {
+			alert('Please enter a prompt');
+		}
+	};
 
-	const handleSubmit = () => {
-		console.log('handleSubmit func');
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (form.prompt && form.photo) {
+			setLoading(true);
+			try {
+				const response = await fetch(
+					'http://localhost:8080/api/v1/post',
+					{
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify(form),
+					}
+				);
+				await response.json();
+				navigate('/');
+			} catch (error) {
+				alert(error)
+			} finally {
+				setLoading(false)
+			}
+		} else {
+			alert('Please enter a prompt and generate an image')
+		}
 	};
 
 	const handleChange = (e) => {
@@ -37,9 +87,8 @@ const CreatePost = () => {
 					Create
 				</h1>
 				<p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
-					Create imaginative and visually stunning images
-					through DALL-E AI and share them with the
-					community
+					Create imaginative and visually stunning images through
+					DALL-E AI and share them with the community
 				</p>
 			</div>
 			<form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
@@ -56,7 +105,7 @@ const CreatePost = () => {
 						labelName='Prompt'
 						type='text'
 						name='prompt'
-						placeholder='Selfie of a German Shepherd with sunglasses in black suit'
+						placeholder='Portrait of a German Shepherd in black suit'
 						value={form.prompt}
 						handleChange={handleChange}
 						isSurpriseMe
